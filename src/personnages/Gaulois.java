bd52447e803c2a8a0e856c513c76571179034b93
Yoshiy0sh <yoshire032@gmail.com>
2023-09-20 15:09:38 +0200

    modif classique

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,6 +1,20 @@
 package personnages;
 
 public class Gaulois {
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	public int getForce() {
+		return force;
+	}
+	public void setForce(int force) {
+		this.force = force;
+	}
+	public int getEffetPotion() {
+		return effetPotion;
+	}
 }
