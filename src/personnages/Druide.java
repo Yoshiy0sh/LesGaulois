8b6664fa3b58b916f5e55fdc7b0c21ccf0ab3ac3
Yoshiy0sh <yoshire032@gmail.com>
2023-09-25 19:40:47 +0200

    le tp 1 presque fini

---
+++ src/personnages/Druide.java
@@ -0,0 +1,66 @@
+package personnages;
+
+import java.util.Random;
+
+public class Druide {
+	private String nom;
+	private int forcePotion;
+	private int effetPotionMin;
+	private int effetPotionMax;
+
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		this.forcePotion = 1;
+		parler("Bonjour je suis le druide " + nom + "et ma potion peut aller d'une force " + effetPotionMin + " à "
+				+ effetPotionMax + ".");
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<<" + texte + ">>");
+
+	}
+
+	private String prendreParole() {
+		return "Le druide" + nom + " : ";
+
+	}
+	public void preparerPotion() {
+		Random random = new Random();
+		forcePotion = random.nextInt(effetPotionMax);
+		while((forcePotion < effetPotionMin) || (forcePotion > effetPotionMax)) 
+		if (forcePotion<effetPotionMin) {
+			forcePotion += 1;
+		} else {
+			forcePotion -= 1;
+
+		}
+		if (forcePotion > 7) {
+			System.out.println("J'ai préparé une super potion de force "+forcePotion);
+			
+		} else {
+			System.out.println("Je n'ai pas trouve tous les ingrédients, ma potion est seulement de force "+forcePotion);
+
+		}
+		
+	}
+	
+	public void booster(Gaulois gaulois) {
+		if (gaulois.getNom() == "Obélix") {
+			parler("Non, Obélix !... Tu n'auras pas de potion magique !");
+		} else {
+			preparerPotion();
+			gaulois.boirePotion(forcePotion);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Druide panoramix = new Druide("Panoramix",5,10);
+		panoramix.preparerPotion();
+	}
+}
--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,37 +1,54 @@
 package personnages;
 
 public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-	private String nom;
-	private int force;
-	private int effetPotion = 1;
-	public int getForce() {
-		return force;
-	}
-	public void setForce(int force) {
-		this.force = force;
-	}
-	public int getEffetPotion() {
-		return effetPotion;
-	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	@Override
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<<" + texte + ">>");
+
+	}
+
+	private String prendreParole() {
+		return "Le gaulois" + nom + " : ";
+	}
+
+	public void frapper(Romain romain) {
+		System.out.println(nom + "envoie un grand coup dans la mâchoire de "+romain.getNom());
+		romain.recevoirCoup((force/3)*effetPotion);
+		
+	}
+	
+	public void boirePotion(int forcePotion) {
+		effetPotion = forcePotion;
+		parler("Merci Druide, je sens que ma force est "+forcePotion+" fois décuplée.");
+		
+		
+	}
+
 	public static void main(String[] args) {
-		switch (key) {
-		case value: {
-			
-			yield type;
-		}
-		default:
-			throw new IllegalArgumentException("Unexpected value: " + key);
-		}
+		Gaulois asterix = new Gaulois("asterix",8);
+//		System.out.println(asterix);
+//		String parole = asterix.prendreParole();
+//		System.out.println(parole);
+//		asterix.parler("j'adore et je suis asterix");
+//		Romain janus = new Romain("janus",12);
+//		asterix.frapper(janus);
+		asterix.boirePotion(3);
 	}
 }
