887552a1ed4d4f63ff9ff07424d1ba7b9c7d0e5c
Yoshiy0sh <yoshire032@gmail.com>
2023-09-25 14:24:07 +0200

    début du projet

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,20 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public int getForce() {
+		return force;
+	}
+	public void setForce(int force) {
+		this.force = force;
+	}
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+}
